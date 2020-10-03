6225fa57bbf2cd61a1ed574f7d1c3e97102b438d
DESKTOP-325D3AA\612110013 <Fzbthai@gmail.com>
2020-10-03 12:51:41 +0700

    first commit

---
+++ 951323_163Midterm-612110013-/Assets/My Asset/Scripts/LockMouse.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockMouse : MonoBehaviour
+{
+    CursorLockMode lockMode;
+ 
+    void Awake () {
+        lockMode = CursorLockMode.Locked;
+        Cursor.lockState = lockMode;
+    }
+}
